81da48bb8ad618efd2e0b47d1731c1c55e891919
Steven Farlie <steven@heroku.com>
2016-07-20 06:38:43 +0000

    README and source

---
+++ dyno-network-stats.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"os"
+	"time"
+
+	linuxproc "github.com/c9s/goprocinfo/linux"
+)
+
+func main() {
+	interval := int64(30)
+	s := os.Getenv("DYNO_NETWORK_STATS_INTERVAL")
+	if s != "" {
+		i, err := strconv.ParseInt(s, 10, 64)
+		if err != nil || i < 1 {
+			fmt.Println("Ignoring DYNO_NETWORK_STATS_INTERVAL value:", s)
+		} else {
+			interval = i
+		}
+	}
+
+	source := os.Getenv("DYNO")
+
+
+	// Stats are differential, so this is the "previous" reading
+	prev, err := linuxproc.ReadNetworkStat("/proc/net/dev")
+	if err != nil {
+		panic(fmt.Sprintf("network stat read fail", err))
+	}
+
+	for {
+		time.Sleep(time.Duration(interval) * time.Second)
+
+		stat, err := linuxproc.ReadNetworkStat("/proc/net/dev")
+		if err != nil {
+			fmt.Println("network stat read fail", err)
+		}
+
+		for _, s := range stat {
+			// Find the previous reading, or default to blank if we haven't seen this interface before
+			p := linuxproc.NetworkStat{s.Iface, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+			for _, x := range prev {
+				if x.Iface == s.Iface {
+					p = x
+				}
+			}
+
+			fmt.Printf("source=%s sample#iface=%s sample#bytes_recv=%d sample#bytes_sent=%d sample#packets_recv=%d sample#packets_sent=%d sample#recv_error=%d sample#sent_error=%d sample#recv_drop=%d sample#sent_drop=%d\n",
+				source,
+				s.Iface,
+				s.RxBytes - p.RxBytes,
+				s.TxBytes - p.TxBytes,
+				s.RxPackets - p.RxPackets,
+				s.TxPackets - p.TxPackets,
+				s.RxErrs - p.RxErrs,
+				s.TxErrs - p.TxErrs,
+				s.RxDrop - p.RxDrop,
+				s.TxDrop - p.TxDrop)
+		}
+
+		prev = stat
+	}
+}
